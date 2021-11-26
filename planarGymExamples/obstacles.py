54d7b1d23a0182c6b439abbd809ee8b0b4fd47f4
Max <m.spahn@tudelft.nl>
2021-11-26 09:45:32 +0100

    Modifies the structure according to new obstacles definition in MotionPlanningScenes

--- planarGymExamples/obstacles.py
+++ planarGymExamples/obstacles.py
@@ -5,10 +5,10 @@ obst1Dict = {
     "type": "sphere",
     "geometry": {"position": [0.0, 5.0], "radius": 1.0},
 }
-sphereObst1 = SphereObstacle("simpleSphere", obst1Dict)
+sphereObst1 = SphereObstacle(name="simpleSphere", contentDict=obst1Dict)
 obst2Dict = {
     "dim": 2,
     "type": "sphere",
     "geometry": {"position": [2.0, -1.0], "radius": 0.2},
 }
-sphereObst2 = SphereObstacle("simpleSphere", obst2Dict)
+sphereObst2 = SphereObstacle(name="simpleSphere", contentDict=obst2Dict)
