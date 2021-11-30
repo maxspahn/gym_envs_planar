65060d7ac986ee4de319aaf2be6b10d9203d39d9
Max <m.spahn@tudelft.nl>
2021-11-30 13:23:26 +0100

    Adds type to goal definition

--- planarGymExamples/goal.py
+++ planarGymExamples/goal.py
@@ -3,7 +3,7 @@ from MotionPlanningGoal.staticSubGoal import StaticSubGoal
 
 goal1Dict = {
     "m": 2, "w": 1.0, "prime": True, 'indices': [0, 1], 'parent_link': 0, 'child_link': 3,
-    'desired_position': [1, 2], 'epsilon': 0.2
+    'desired_position': [1, 2], 'epsilon': 0.2, 'type': "staticSubGoal", 
 }
 
 goal1 = StaticSubGoal(name="goal1", contentDict=goal1Dict)
