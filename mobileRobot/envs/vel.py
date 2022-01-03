f9adc0104925450c569eeaf678e0056aba16f9ea
Alex <alexanderschwarz.ulm@gmail.com>
2022-01-03 14:31:57 +0100

    Updates mobileRobot according to modifications in planarCommon. Integration of observation space dictionaries for vel and acc environments of the mobileRobot.

--- mobileRobot/envs/vel.py
+++ mobileRobot/envs/vel.py
@@ -2,19 +2,7 @@ import numpy as np
 from gym import spaces
 
 from mobileRobot.envs.mobileRobotEnv import MobileRobotEnv
+from planarCommon.velEnv import VelEnv
 
-class MobileRobotVelEnv(MobileRobotEnv):
-
-    def setSpaces(self):
-        o = np.concatenate((self._limUpPos, self._limUpVel))
-        self.observation_space = spaces.Box(low=-o, high=o, dtype=np.float64)
-        self.action_space = spaces.Box(
-            low=-self._limUpAcc, high=self._limUpAcc, dtype=np.float64
-        )
-
-    def continuous_dynamics(self, x, t):
-        self.state[self._n: 2 * self._n] = self.action
-        vel = self.action
-        acc = np.zeros(self._n)
-        xdot = np.concatenate((vel, acc))
-        return xdot
+class MobileRobotVelEnv(VelEnv, MobileRobotEnv):
+    pass
