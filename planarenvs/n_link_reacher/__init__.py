8e1661af29b82dc8c482842c4d17e7386e1152b3
Max Spahn <m.spahn@tudelft.nl>
2022-07-27 12:04:36 +0200

    Removes torque environment to remove casadi dependency.

--- planarenvs/n_link_reacher/__init__.py
+++ planarenvs/n_link_reacher/__init__.py
@@ -8,7 +8,3 @@ register(
     id="nLink-reacher-vel-v0",
     entry_point="planarenvs.n_link_reacher.envs:NLinkVelReacherEnv",
 )
-register(
-    id="nLink-reacher-tor-v0",
-    entry_point="planarenvs.n_link_reacher.envs:NLinkTorReacherEnv",
-)
