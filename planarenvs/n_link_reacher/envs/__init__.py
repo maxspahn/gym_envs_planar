41fe4f7d998bd81ff7fbea4ed901dc80f16b9685
Max Spahn <m.spahn@tudelft.nl>
2022-07-27 14:58:56 +0200

    Removes torque environment to remove casadi dependency. (#55)
    * Removes torque environment to remove casadi dependency.
    
    * Removes scenes extra from ci testing.

--- planarenvs/n_link_reacher/envs/__init__.py
+++ planarenvs/n_link_reacher/envs/__init__.py
@@ -1,3 +1,2 @@
 from planarenvs.n_link_reacher.envs.acc import NLinkAccReacherEnv
 from planarenvs.n_link_reacher.envs.vel import NLinkVelReacherEnv
-from planarenvs.n_link_reacher.envs.tor import NLinkTorReacherEnv
